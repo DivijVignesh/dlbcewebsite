64df065561d46263f4b91097670bf269f4c6746d
Divij Vignesh <divijvignesh12@gmail.com>
2022-02-20 23:08:45 +0530

    update

--- dlbcewebsite/urls.py
+++ dlbcewebsite/urls.py
@@ -19,9 +19,10 @@ from django.urls import path
 from django.views.static import  serve
 from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
